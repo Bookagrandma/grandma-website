ad62668ec77e1be89c9e083665b47d75167e58ca
Raouphy <albertgnae@hotmail.fr>
2017-12-05 18:48:33 +0100

    fixe sin up

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,11 @@
 Rails.application.routes.draw do
- 
-  devise_for :etablissements
+
+
   devise_for :benevoles
-  resources :missions
-  resources :etablissements
-  resources :benevoles
-  resources :comments
-  # devise_for :benevoles
-  # devise_for :etablissements
 
-resources :etablissements do
-	resources :missions
-end
-
-resources :benevoles do
-	resources :missions
-end
-
-resources :missions do
-	resources :etablissements
-end
-
-resources :missions do
-	resources :benevoles
-end
-
-resources :comments do
-	resources :missions
-end
 
 root 'home#index'
 
 
 
 end
-
