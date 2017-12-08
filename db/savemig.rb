ecb1e04ea6007f928bf8a5fdb0b3c024b32aaa29
Etienne Bouron <etienne.toutbien@gmail.com>
2017-12-08 18:30:16 +0100

    drop table etablissement

---
+++ db/savemig.rb
@@ -0,0 +1,20 @@
+create_table "etablissements", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "zip"
+    t.string "city"
+    t.string "category"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "dept"
