91e54679ea4873448d1ab66156093255bdbb0baa
Etienne Bouron <etienne.toutbien@gmail.com>
2017-12-13 12:02:05 +0100

    successfull carrierwave

--- app/controllers/benevoles_controller.rb
+++ app/controllers/benevoles_controller.rb
@@ -1,5 +1,9 @@
 class BenevolesController < ApplicationController
 
+	before_action :authenticate_benevole!, only: [:edit, :update]
+	before_action :check_authorization, only: [:edit, :update]
+	before_action :set_benevole
+
 	def index
 		@currentBenevole = current_benevole.id
 	end
@@ -15,13 +19,15 @@ class BenevolesController < ApplicationController
     respond_to do |format|
       if @benevole.update(benevole_params)
         format.html { redirect_to @benevole, notice: 'Benevole was successfully updated.' }
-        format.json { render :show, status: :ok, location: @benevole }
+        # format.json { render :show, status: :ok, location: @benevole }
       else
         format.html { render :edit }
-        format.json { render json: @benevole.errors, status: :unprocessable_entity }
+        flash.new[:alert] = "Something went wrong. Please try again"
+
+        # format.json { render json: @benevole.errors, status: :unprocessable_entity }
       end
-    end
-  end 
+      end
+    end 
 
   private
       # Use callbacks to share common setup or constraints between actions.
@@ -29,10 +35,16 @@ class BenevolesController < ApplicationController
         @benevole = Benevole.find(params[:id])
       end
 
+      def check_authorization
+      	unless current_benevole.id == params[:id].to_i
+      		redirect_to root_url
+      end
+
       #Never trust parameters from the scary internet, only allow the white list through.
       def benevole_params
         params.require(:benevole).permit(:firstname, :lastname, :birthday, :address, :zip, :city, :gender, :avatar, :email, :phone, :motivation)
       end	
+    end
 end
 
 
